ced324f723bea8231ac0b00b163f523f549b002d
tuhin <mstuhin999@gmail.com>
2024-12-25 23:26:26 +0600

    nev add

--- lib/main.dart
+++ lib/main.dart
@@ -22,40 +22,61 @@ class HomeActivity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("hello"),
-        centerTitle: true,
-        backgroundColor: Colors.pink,
-        actions: [
-          Icon(
-            Icons.search,
-          )
-        ],
-      ),
-      body: Text(
-        'hello wold',
-        textAlign: TextAlign.center,
-      ),
-      drawer: Drawer(
-        backgroundColor: Colors.amber[100],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.pink,
-        onPressed: () {},
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Contact'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
-        ],
-        onTap: (int index) {
-          if (index == 0) {}
-          if (index == 1) {}
-          if (index == 2) {}
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text("hello"),
+          centerTitle: true,
+          backgroundColor: Colors.pink,
+          actions: [
+            Icon(
+              Icons.search,
+            )
+          ],
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.amber[100],
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                  accountName: Text('Tuhin'),
+                  accountEmail: Text('mstuhin@gmail.com'),
+                  currentAccountPicture: CircleAvatar()),
+              ListTile(
+                title: Text('Home'),
+                leading: Icon(Icons.home),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: Colors.pink,
+          onPressed: () {},
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.message), label: 'Contact'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+          ],
+          onTap: (int index) {
+            if (index == 0) {}
+            if (index == 1) {}
+            if (index == 2) {}
+          },
+        ),
+        body: Container(
+            height: 40,
+            width: 300,
+            margin: EdgeInsets.fromLTRB(37, 40, 35, 10),
+            decoration: BoxDecoration(
+                color: Colors.pink, borderRadius: BorderRadius.circular(10)),
+            child: Text(
+              'Switch to service provider',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            )));
   }
 }
